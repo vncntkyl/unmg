28b5b400024ff7e3c5fb4d1339be41d832792b91
vncntkyl <kyle.rinoza2009@gmail.com>
2023-04-18 09:19:46 +0800

    added some functions

--- src/context/FunctionContext.jsx
+++ src/context/FunctionContext.jsx
@@ -8,6 +8,14 @@ export function FunctionProvider({ children }) {
   function capitalize(str) {
     return str.charAt(0).toUpperCase() + str.slice(1);
   }
+  function capitalizeSentence(sentence) {
+    let splitSentence = sentence.split(" ");
+    let joined = [];
+    splitSentence.forEach((word) => {
+      joined.push(capitalize(word));
+    });
+    return joined.join(" ");
+  }
   function splitKey(key) {
     let tempString = [];
     const str = key.split("_");
@@ -36,16 +44,45 @@ export function FunctionProvider({ children }) {
   }
 
   function areValuesFilled(obj) {
-    const values = Object.values(obj);
+    const keysToSkip = ["middle_name", "immediate_supervisor"];
+    const values = Object.keys(obj)
+      .filter((key) => !keysToSkip.includes(key))
+      .map((key) => obj[key]);
     return values.every((value) => value !== "");
   }
-
   function formatName(name) {
     return name.split(" ").join("_");
   }
 
-  function reformatName(name){
+  function reformatName(name) {
     return name.split("_").join(" ");
+  }
+  function compareObjectArrays(obj1, obj2) {
+    const differences = [];
+
+    for (let key in obj1) {
+      if (obj1.hasOwnProperty(key) && obj2.hasOwnProperty(key)) {
+        if (obj1[key] !== obj2[key]) {
+          differences.push({
+            key: key,
+            oldValue: obj1[key],
+            newValue: obj2[key]
+          });
+        }
+      }
+    }
+  
+    for (let key in obj2) {
+      if (obj2.hasOwnProperty(key) && !obj1.hasOwnProperty(key)) {
+        differences.push({
+          key: key,
+          oldValue: undefined,
+          newValue: obj2[key]
+        });
+      }
+    }
+  
+    return differences;
   }
   const value = {
     getPath,
@@ -56,6 +93,8 @@ export function FunctionProvider({ children }) {
     reformatName,
     capitalizePath,
     areValuesFilled,
+    capitalizeSentence,
+    compareObjectArrays,
   };
 
   return (
--- src/context/authContext.jsx
+++ src/context/authContext.jsx
@@ -9,9 +9,12 @@ export function useAuth() {
 export function AuthProvider({ children }) {
   const [currentUser, setCurrentUser] = useState();
   const [companyList, setCompanyList] = useState([]);
+  const [headList, setHeadList] = useState([]);
+  const [usertypeList, setUsertypeList] = useState([]);
+  const [departmentList, setDepartmentList] = useState([]);
   const nav = useNavigate();
 
-  function signInUser(username, password) {
+  const signInUser = (username, password) => {
     const url = "http://localhost/unmg_pms/api/signInUser.php";
     //const url = "../api/signInUser.php";
     let formData = new FormData();
@@ -26,31 +29,142 @@ export function AuthProvider({ children }) {
         nav("/");
       })
       .catch((e) => {
-        alert(e);
+        console.log(e.message);
       });
+  };
+
+  const registerUser = async (userdata) => {
+    try {
+      const url = "http://localhost/unmg_pms/api/registerUser.php";
+      //const url = "../api/registerUser.php";
+      const response = await axios.post(url, {
+        params: {
+          userdata: userdata,
+        },
+      });
+
+      if (response.data === "success") {
+        return true;
+      }
+    } catch (e) {
+      console.log(e.message);
+    }
+  };
+
+  const updateUser = async (userdata, id) => {
+    try {
+      const url = "http://localhost/unmg_pms/api/updateUser.php";
+      //const url = "../api/updateUser.php";
+      const response = await axios.post(url, {
+        params: {
+          userdata: userdata,
+          id: id
+        },
+      });
+      if (response.data === "success") {
+        return true;
+      }
+    } catch (e) {
+      console.log(e.message);
+    }
+  };
+
+  const manageUser = async (action, id) => {
+    const url = "http://localhost/unmg_pms/api/userActions.php";
+    //const url = "../api/userActions.php";
+    const formData = new FormData();
+    formData.append("action", action);
+    formData.append("user_id", id)
+    
+    try {
+      const response = await axios.post(url, formData);
+      if(response.data === "success"){
+        return true;
+      }
+    } catch (e) {
+      console.log(e)
+    }
   }
-  
+  const navigate = (location) => {
+    nav(location);
+  };
   useEffect(() => {
     if (sessionStorage.getItem("currentUser")) {
       setCurrentUser(sessionStorage.getItem("currentUser"));
     }
-    //const url = "../api/conglomerate.php";
     const url = "http://localhost/unmg_pms/api/conglomerate.php";
+    //const url = "../api/conglomerate.php";
+
     axios
-      .get(url)
+      .get(url, {
+        params: {
+          company: true,
+        },
+      })
       .then((response) => {
         setCompanyList(response.data);
       })
       .catch((e) => {
-        console.log(e);
+        console.log(e.message);
       });
+
+    axios
+      .get(url, {
+        params: {
+          department: true,
+        },
+      })
+      .then((response) => {
+        setDepartmentList(response.data);
+      })
+      .catch((e) => {
+        console.log(e.message);
+      });
+    const fetchHeads = async () => {
+      let url = "http://localhost/unmg_pms/api/retrieveUsers.php";
+      //let url = "../api/retrieveUsers.php";
+      try {
+        const response = await axios.get(url, {
+          params: {
+            heads: true,
+          },
+        });
+
+        setHeadList(response.data);
+      } catch (e) {
+        console.log(e.message);
+      }
+    };
+    const retrieveUserTypes = async () => {
+      try {
+        const url = "http://localhost/unmg_pms/api/retrieveUsers.php";
+        //const url = "../api/retrieveUsers.php";
+        const response = await axios.get(url, {
+          params: {
+            usertype: true,
+          },
+        });
+        setUsertypeList(response.data);
+      } catch (e) {
+        console.log(e.message);
+      }
+    };
+    fetchHeads();
+    retrieveUserTypes();
   }, [currentUser]);
 
   const value = {
     currentUser,
     companyList,
+    departmentList,
+    headList,
+    usertypeList,
     nav,
+    navigate,
+    manageUser,
     signInUser,
+    updateUser,
+    registerUser,
     setCurrentUser,
   };
 
