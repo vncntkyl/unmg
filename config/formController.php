3314eaa3db5ffca1a58e66357a6ace2fbae8cdd2
vincentaspan14 <vincentaspan14@gmail.com>
2023-04-28 16:59:06 +0800

    updates for second part of evaluation form

--- config/formController.php
+++ config/formController.php
@@ -41,30 +41,30 @@ Class formController extends Controller
     }
     function GoalsCount($formID)
     {
-        $this->setStatement("SELECT COUNT(*) as `Count` FROM `hr_objectives` WHERE hr_eval_form_fp_id = ':formID'");
+        $this->setStatement("SELECT COUNT(*) as `Count` FROM `hr_objectives` WHERE hr_eval_form_fp_id = :formID");
         $this->statement->execute([':formID' => $formID]);
         return $this->statement->fetch();
     }
     function LastGoalsID()
     {
-        $this->setStatement("SELECT objective_id FROM `hr_objectives` order by objective_id DESC LIMIT 1");;
+        $this->setStatement("SELECT objective_id FROM `hr_objectives` order by objective_id DESC LIMIT 1");
         $this->statement->execute();
         return $this->statement->fetch();
     }
     function insertKPI($kpiDesc, $objectID,$weight)
     {
-        $this->setStatement("INSERT into `hr_kpi` (kpi_desc,objective_id,kpi_weight) VALUES (:kpi_desc,:objectID,:weight)'");
+        $this->setStatement("INSERT into `hr_kpi` (kpi_desc,objective_id,kpi_weight) VALUES (:kpi_desc,:objectID,:weight)");
         return $this->statement->execute([':kpi_desc' => $kpiDesc, ':objectID' => $objectID,':weight' => $weight]);
     }
     function kpiCount($objID)
     {
-        $this->setStatement("SELECT COUNT(*) as `Count` FROM `hr_kpi` WHERE objective_id = ':objID'");
+        $this->setStatement("SELECT COUNT(*) as `Count` FROM `hr_kpi` WHERE objective_id = :objID");
         $this->statement->execute([':objID' => $objID]);
         return $this->statement->fetch();
     }
     function LastKpiID()
     {
-        $this->setStatement("SELECT kpi_id FROM `hr_kpi` order by kpi_id DESC LIMIT 1");;
+        $this->setStatement("SELECT kpi_id FROM `hr_kpi` order by kpi_id DESC LIMIT 1");
         $this->statement->execute();
         return $this->statement->fetch();
     }
@@ -104,18 +104,28 @@ Class formController extends Controller
     }
     function insertTargetMetrics($TMScore,$TMDesc,$kpiID)
     {
-        $this->setStatement("INSERT into `hr_target_metrics` (target_metrics_score,target_metrics_desc,kpi_id) VALUES (:TMScore,:TMDesc,:kpiID)'");
+        $this->setStatement("INSERT into `hr_target_metrics` (target_metrics_score,target_metrics_desc,kpi_id) VALUES (:TMScore,:TMDesc,:kpiID)");
         return $this->statement->execute([':TMScore' => $TMScore, ':TMDesc' => $TMDesc,':kpiID' => $kpiID]);
     }
     function insertFqEval($formID,$results,$remarks,$reviewDate)
     {
-        $this->setStatement("INSERT into `hr_eval_form_sp_fq` (hr_eval_form_id,results,remarks,fq_review_date) VALUES (:formID,:results,:remarks,:revDate)'");
+        $this->setStatement("INSERT into `hr_eval_form_sp_fq` (hr_eval_form_id,results,remarks,fq_review_date) VALUES (:formID,:results,:remarks,:revDate)");
         return $this->statement->execute([':formID' => $formID, ':results' => $results,':remarks' => $remarks,':revDate' => $reviewDate]);
     }
-    function insertMyrEval($formID,$results,$status,$remarks,$reviewDate)
+    function insertMyrEval($formID,$results,$status,$remarks,$reviewDate,$actToAddress)
     {
-        $this->setStatement("INSERT into `hr_eval_form_sp_fq` (hr_eval_form_id,results,status,remarks,fq_review_date) VALUES (:formID,:results,:status,:remarks,:revDate)'");
-        return $this->statement->execute([':formID' => $formID, ':results' => $results,':remarks' => $remarks,':status' => $status,':revDate' => $reviewDate]);
+        $this->setStatement("INSERT into `hr_eval_form_sp_myr` (eval_form_id,results,status,remarks,actions_to_address,myr_review_date) VALUES (:formID,:results,:status,:remarks,:actAddress,:revDate)");
+        return $this->statement->execute([':formID' => $formID, ':results' => $results,':remarks' => $remarks,':status' => $status,':actAddress'=>$actToAddress,':revDate' => $reviewDate]);
+    }
+    function insertTqEval($formID,$results,$reVariance,$reviewDate)
+    {
+        $this->setStatement("INSERT into `hr_eval_form_sp_tq` (eval_form_id,results,reason_for_variance,tq_review_date) VALUES (:formID,:results,:reVariance,:revDate)");
+        return $this->statement->execute([':formID' => $formID, ':results' => $results,':reVariance' => $reVariance,':revDate' => $reviewDate]);
+    }
+    function insertYeEval($formID,$results,$remarks,$reviewDate)
+    {
+        $this->setStatement("INSERT into `hr_eval_form_sp_yee` (eval_form_id,results,remarks,yee_review_date) VALUES (:formID,:results,:remarks,:revDate)");
+        return $this->statement->execute([':formID' => $formID, ':results' => $results,':remarks' => $remarks,':revDate' => $reviewDate]);
     }
     
 
