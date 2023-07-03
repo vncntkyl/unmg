886c46061c4023bcaf3710147d4c61721a6d1aa5
Aztromi <norvinkyle@gmail.com>
2023-07-03 19:04:26 +0800

    fgdfg

--- src/components/TrackingAssessment/EmployeeAssessment.jsx
+++ src/components/TrackingAssessment/EmployeeAssessment.jsx
@@ -2,11 +2,12 @@ import React, { useState, useEffect } from 'react';
 import axios from "axios";
 import { error } from 'jquery';
 import Badge from '../../misc/Badge';
-import Toggle from '../Toggle';
+import classNames from 'classnames';
 
 export default function EmployeeAssessment() {
     const employee_id = sessionStorage.getItem("assessment_id");
-    const [panel, setPanel] = useState("My Assessment");
+    const 
+
 
 
 
@@ -21,13 +22,38 @@ export default function EmployeeAssessment() {
                         Norvin Perez
                     </label>
                 </div>
-                <Toggle
-                paths={["/employee_assessment/", "/employee_assessment"]}
-                panel={panel}
-                panel_1={"Achievements"}
-                setPanel={setPanel}
-                panel_2={"Grades"}
-              />
+
+
+        <div
+          className={classNames(
+            "toggle flex flex-row gap-2 bg-default w-full p-1 rounded-full relative overflow-hidden z-[4] md:w-[400px]",
+            panel !== panel_1 && "on"
+          )}
+        >
+          <button
+            type="button"
+            className={classNames(
+              "toggle_text py-1 px-2 rounded-full text-[.8rem] z-[6] text-center w-1/2 md:text-[.8rem]",
+              panel === panel_1 ? "text-white" : "text-black"
+            )}
+            // onClick=();
+          >
+            Achievements
+          </button>
+          <button
+            type="button"
+            className={classNames(
+              "toggle_text py-1 px-2 rounded-full text-[.8rem] z-[6] w-1/2 text-center whitespace-nowrap md:text-[.8rem]",
+              panel === panel_2 ? "text-white" : "text-black"
+            )}
+            // onClick=();
+          >
+            Grades
+          </button>
+        </div>
+
+
+
             </div>
             <div className="flex pb-2 px-2 justify-between">
                 <div className="flex flex-row items-center gap-2 justify-between md:justify-start">
