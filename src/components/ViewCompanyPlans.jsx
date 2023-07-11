51f618bc3f568308d0660009ba6602fbad657e68
vincentaspan14 <67308438+vincentaspan14@users.noreply.github.com>
2023-07-11 12:58:19 +0800

    Update ViewCompanyPlans.jsx

--- src/components/ViewCompanyPlans.jsx
+++ src/components/ViewCompanyPlans.jsx
@@ -8,6 +8,8 @@ const ViewCompanyPlans = ({
   selectedDepartmentID,
   employeeType,
   query,
+  selectedKpiDuration,
+  currentUserType,
 }) => {
   const [fetchedPillarOrObjectives, setFetchedPillarOrObjectives] = useState(
     []
@@ -26,20 +28,20 @@ const ViewCompanyPlans = ({
         const axiosFetchedData = response.data;
         const displayedPillarEmployeeIds = new Set();
         const extractedObjectivesOrPillar = [];
-  
+
         for (let i = 0; i < axiosFetchedData.length; i += 4) {
           const batch = axiosFetchedData.slice(i, i + 4);
           const uniqueBatch = [];
-          
+
           for (const pillar of batch) {
             const pillarEmployeeId = pillar.employee_id;
             const pillarIdCombination = `${pillar.pillar_id}-${pillarEmployeeId}`;
-            
+
             if (!displayedPillarEmployeeIds.has(pillarIdCombination)) {
               displayedPillarEmployeeIds.add(pillarIdCombination);
               uniqueBatch.push(pillar);
             }
-            
+
             if (uniqueBatch.length === 4) {
               break;
             }
@@ -51,14 +53,341 @@ const ViewCompanyPlans = ({
         console.log(error.message);
       }
     };
-  
+
     fetchPillarOrObjectives();
   }, [selectedYear]);
-  
-  
 
   const RenderTable = () => {
     if (filterSelectedObjectiveOrPillar === "Pillar Percentage") {
+      if (currentUserType <= 2) {
+        if (selectedKpiDuration == "All") {
+          return (
+            <table className="table-auto w-full">
+              <thead className="bg-un-blue-light text-white">
+                <tr>
+                  {[
+                    "Name",
+                    "Job Title",
+                    "Financial Bottomline",
+                    "Customer Delight",
+                    "Operational Excellence",
+                    "Organizational Capacity",
+                  ].map((header, index) => (
+                    <th
+                      key={index}
+                      className="text-center bg-gray-200 px-4 py-2"
+                    >
+                      {header}
+                    </th>
+                  ))}
+                </tr>
+              </thead>
+              <tbody>
+                {fetchedPillarOrObjectives.forEach((batch) =>
+                  batch.forEach((row) => {
+                    const existingEntry = uniqueEntries.find(
+                      (entry) =>
+                        entry.Job_Title === row.Job_Title &&
+                        entry.Name === row.Name
+                    );
+
+                    if (!existingEntry) {
+                      uniqueEntries.push({
+                        Job_Title: row.Job_Title,
+                        Name: row.Name,
+                      });
+                    }
+
+                    const existingProcessedDataEntry = processedData.find(
+                      (entry) =>
+                        entry.Job_Title === row.Job_Title &&
+                        entry.Name === row.Name
+                    );
+
+                    if (existingProcessedDataEntry) {
+                      for (const key in row) {
+                        if (
+                          key !== "Job_Title" &&
+                          key !== "Name" &&
+                          key.includes("pillar_percentage")
+                        ) {
+                          existingProcessedDataEntry[key] = [
+                            ...(existingProcessedDataEntry[key] || []),
+                            row[key],
+                          ]; // Store the quarter value as an array of numbers
+                        }
+                      }
+                    } else {
+                      const processedRow = {
+                        Job_Title: row.Job_Title,
+                        Name: row.Name,
+                        ...row,
+                      };
+
+                      for (const key in processedRow) {
+                        if (
+                          key !== "Job_Title" &&
+                          key !== "Name" &&
+                          key.includes("pillar_percentage")
+                        ) {
+                          processedRow[key] = [processedRow[key]]; // Store the quarter value as an array of numbers
+                        }
+                      }
+                      processedData.push(processedRow);
+                    }
+                  })
+                )}
+                {processedData
+                  .filter(
+                    (row) =>
+                      row.contract_type === employeeType &&
+                      (row.company == selectedCompanyID ||
+                        selectedCompanyID == "All") &&
+                      (row.department == selectedDepartmentID ||
+                        selectedDepartmentID == "All") &&
+                      row.EvalPillarID == row.pillar_id &&
+                      row.Name.toLowerCase().includes(query)
+                  )
+                  .map((row, rowIndex) => (
+                    <tr key={rowIndex}>
+                      <td className="text-center bg-gray-200 px-4 py-2">
+                        {row.Name}
+                      </td>
+                      <td className="text-center bg-gray-200 px-4 py-2">
+                        {row.Job_Title}
+                      </td>
+                      {row.pillar_percentage
+                        .slice(0, 4)
+                        .map((rating, index) => (
+                          <td
+                            className="text-center bg-gray-200 px-4 py-2"
+                            key={index}
+                          >
+                            {rating}%
+                          </td>
+                        ))}
+                    </tr>
+                  ))}
+              </tbody>
+            </table>
+          );
+        }
+        return (
+          <table className="table-auto w-full">
+            <thead className="bg-un-blue-light text-white">
+              <tr>
+                {[
+                  "Name",
+                  "Job Title",
+                  "Financial Bottomline",
+                  "Customer Delight",
+                  "Operational Excellence",
+                  "Organizational Capacity",
+                ].map((header, index) => (
+                  <th key={index} className="text-center bg-gray-200 px-4 py-2">
+                    {header}
+                  </th>
+                ))}
+              </tr>
+            </thead>
+            <tbody>
+              {fetchedPillarOrObjectives.forEach((batch) =>
+                batch.forEach((row) => {
+                  const existingEntry = uniqueEntries.find(
+                    (entry) =>
+                      entry.Job_Title === row.Job_Title &&
+                      entry.Name === row.Name
+                  );
+
+                  if (!existingEntry) {
+                    uniqueEntries.push({
+                      Job_Title: row.Job_Title,
+                      Name: row.Name,
+                    });
+                  }
+
+                  const existingProcessedDataEntry = processedData.find(
+                    (entry) =>
+                      entry.Job_Title === row.Job_Title &&
+                      entry.Name === row.Name
+                  );
+
+                  if (existingProcessedDataEntry) {
+                    for (const key in row) {
+                      if (
+                        key !== "Job_Title" &&
+                        key !== "Name" &&
+                        key.includes("pillar_percentage")
+                      ) {
+                        existingProcessedDataEntry[key] = [
+                          ...(existingProcessedDataEntry[key] || []),
+                          row[key],
+                        ]; // Store the quarter value as an array of numbers
+                      }
+                    }
+                  } else {
+                    const processedRow = {
+                      Job_Title: row.Job_Title,
+                      Name: row.Name,
+                      ...row,
+                    };
+
+                    for (const key in processedRow) {
+                      if (
+                        key !== "Job_Title" &&
+                        key !== "Name" &&
+                        key.includes("pillar_percentage")
+                      ) {
+                        processedRow[key] = [processedRow[key]]; // Store the quarter value as an array of numbers
+                      }
+                    }
+                    processedData.push(processedRow);
+                  }
+                })
+              )}
+              {processedData
+                .filter(
+                  (row) =>
+                    row.form_kpi_duration == selectedKpiDuration &&
+                    row.contract_type === employeeType &&
+                    (row.company == selectedCompanyID ||
+                      selectedCompanyID == "All") &&
+                    (row.department == selectedDepartmentID ||
+                      selectedDepartmentID == "All") &&
+                    row.EvalPillarID == row.pillar_id &&
+                    row.Name.toLowerCase().includes(query)
+                )
+                .map((row, rowIndex) => (
+                  <tr key={rowIndex}>
+                    <td className="text-center bg-gray-200 px-4 py-2">
+                      {row.Name}
+                    </td>
+                    <td className="text-center bg-gray-200 px-4 py-2">
+                      {row.Job_Title}
+                    </td>
+                    {row.pillar_percentage.slice(0, 4).map((rating, index) => (
+                      <td
+                        className="text-center bg-gray-200 px-4 py-2"
+                        key={index}
+                      >
+                        {rating}%
+                      </td>
+                    ))}
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+        );
+      }
+    }
+    if (currentUserType <= 2) {
+      if (selectedKpiDuration == "All") {
+        return (
+          <table className="table-auto w-full">
+            <thead className="bg-un-blue-light text-white">
+              <tr>
+                {[
+                  "Name",
+                  "Job Title",
+                  "Financial Bottomline",
+                  "Customer Delight",
+                  "Operational Excellence",
+                  "Organizational Capacity",
+                ].map((header, index) => (
+                  <th key={index} className="text-center bg-gray-200 px-4 py-2">
+                    {header}
+                  </th>
+                ))}
+              </tr>
+            </thead>
+            <tbody>
+              {fetchedPillarOrObjectives.forEach((batch) =>
+                batch.forEach((row) => {
+                  const existingEntry = uniqueEntries.find(
+                    (entry) =>
+                      entry.Job_Title === row.Job_Title &&
+                      entry.Name === row.Name
+                  );
+
+                  if (!existingEntry) {
+                    uniqueEntries.push({
+                      Job_Title: row.Job_Title,
+                      Name: row.Name,
+                    });
+                  }
+
+                  const existingProcessedDataEntry = processedData.find(
+                    (entry) =>
+                      entry.Job_Title === row.Job_Title &&
+                      entry.Name === row.Name
+                  );
+
+                  if (existingProcessedDataEntry) {
+                    for (const key in row) {
+                      if (
+                        key !== "Job_Title" &&
+                        key !== "Name" &&
+                        key.includes("objective")
+                      ) {
+                        existingProcessedDataEntry[key] = [
+                          ...(existingProcessedDataEntry[key] || []),
+                          row[key],
+                        ]; // Store the quarter value as an array of numbers
+                      }
+                    }
+                  } else {
+                    const processedRow = {
+                      Job_Title: row.Job_Title,
+                      Name: row.Name,
+                      ...row,
+                    };
+
+                    for (const key in processedRow) {
+                      if (
+                        key !== "Job_Title" &&
+                        key !== "Name" &&
+                        key.includes("objective")
+                      ) {
+                        processedRow[key] = [processedRow[key]]; // Store the quarter value as an array of numbers
+                      }
+                    }
+                    processedData.push(processedRow);
+                  }
+                })
+              )}
+              {processedData
+                .filter(
+                  (row) =>
+                    row.contract_type === employeeType &&
+                    (row.company == selectedCompanyID ||
+                      selectedCompanyID == "All") &&
+                    (row.department == selectedDepartmentID ||
+                      selectedDepartmentID == "All") &&
+                    row.EvalPillarID == row.pillar_id &&
+                    row.Name.toLowerCase().includes(query)
+                )
+                .map((row, rowIndex) => (
+                  <tr key={rowIndex}>
+                    <td className="text-center bg-gray-200 px-4 py-2">
+                      {row.Name}
+                    </td>
+                    <td className="text-center bg-gray-200 px-4 py-2">
+                      {row.Job_Title}
+                    </td>
+                    {row.objective.slice(0, 4).map((rating, index) => (
+                      <td
+                        className="text-center bg-gray-200 px-4 py-2"
+                        key={index}
+                      >
+                        {rating}
+                      </td>
+                    ))}
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+        );
+      }
       return (
         <table className="table-auto w-full">
           <thead className="bg-un-blue-light text-white">
@@ -102,7 +431,7 @@ const ViewCompanyPlans = ({
                     if (
                       key !== "Job_Title" &&
                       key !== "Name" &&
-                      key.includes("pillar_percentage")
+                      key.includes("objective")
                     ) {
                       existingProcessedDataEntry[key] = [
                         ...(existingProcessedDataEntry[key] || []),
@@ -121,7 +450,7 @@ const ViewCompanyPlans = ({
                     if (
                       key !== "Job_Title" &&
                       key !== "Name" &&
-                      key.includes("pillar_percentage")
+                      key.includes("objective")
                     ) {
                       processedRow[key] = [processedRow[key]]; // Store the quarter value as an array of numbers
                     }
@@ -133,6 +462,7 @@ const ViewCompanyPlans = ({
             {processedData
               .filter(
                 (row) =>
+                  row.form_kpi_duration == selectedKpiDuration &&
                   row.contract_type === employeeType &&
                   (row.company == selectedCompanyID ||
                     selectedCompanyID == "All") &&
@@ -149,12 +479,12 @@ const ViewCompanyPlans = ({
                   <td className="text-center bg-gray-200 px-4 py-2">
                     {row.Job_Title}
                   </td>
-                  {row.pillar_percentage.slice(0, 4).map((rating, index) => (
+                  {row.objective.slice(0, 4).map((rating, index) => (
                     <td
                       className="text-center bg-gray-200 px-4 py-2"
                       key={index}
                     >
-                      {rating}%
+                      {rating}
                     </td>
                   ))}
                 </tr>
@@ -163,109 +493,6 @@ const ViewCompanyPlans = ({
         </table>
       );
     }
-    return (
-      <table className="table-auto w-full">
-        <thead className="bg-un-blue-light text-white">
-          <tr>
-            {[
-              "Name",
-              "Job Title",
-              "Financial Bottomline",
-              "Customer Delight",
-              "Operational Excellence",
-              "Organizational Capacity",
-            ].map((header, index) => (
-              <th key={index} className="text-center bg-gray-200 px-4 py-2">
-                {header}
-              </th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {fetchedPillarOrObjectives.forEach((batch) =>
-            batch.forEach((row) => {
-              const existingEntry = uniqueEntries.find(
-                (entry) =>
-                  entry.Job_Title === row.Job_Title && entry.Name === row.Name
-              );
-
-              if (!existingEntry) {
-                uniqueEntries.push({
-                  Job_Title: row.Job_Title,
-                  Name: row.Name,
-                });
-              }
-
-              const existingProcessedDataEntry = processedData.find(
-                (entry) =>
-                  entry.Job_Title === row.Job_Title && entry.Name === row.Name
-              );
-
-              if (existingProcessedDataEntry) {
-                for (const key in row) {
-                  if (
-                    key !== "Job_Title" &&
-                    key !== "Name" &&
-                    key.includes("objective")
-                  ) {
-                    existingProcessedDataEntry[key] = [
-                      ...(existingProcessedDataEntry[key] || []),
-                      row[key],
-                    ]; // Store the quarter value as an array of numbers
-                  }
-                }
-              } else {
-                const processedRow = {
-                  Job_Title: row.Job_Title,
-                  Name: row.Name,
-                  ...row,
-                };
-
-                for (const key in processedRow) {
-                  if (
-                    key !== "Job_Title" &&
-                    key !== "Name" &&
-                    key.includes("objective")
-                  ) {
-                    processedRow[key] = [processedRow[key]]; // Store the quarter value as an array of numbers
-                  }
-                }
-                processedData.push(processedRow);
-              }
-            })
-          )}
-          {processedData
-            .filter(
-              (row) =>
-                row.contract_type === employeeType &&
-                (row.company == selectedCompanyID ||
-                  selectedCompanyID == "All") &&
-                (row.department == selectedDepartmentID ||
-                  selectedDepartmentID == "All") &&
-                row.EvalPillarID == row.pillar_id &&
-                row.Name.toLowerCase().includes(query)
-            )
-            .map((row, rowIndex) => (
-              <tr key={rowIndex}>
-                <td className="text-center bg-gray-200 px-4 py-2">
-                  {row.Name}
-                </td>
-                <td className="text-center bg-gray-200 px-4 py-2">
-                  {row.Job_Title}
-                </td>
-                {row.objective.slice(0, 4).map((rating, index) => (
-                  <td
-                    className="text-center bg-gray-200 px-4 py-2"
-                    key={index}
-                  >
-                    {rating}
-                  </td>
-                ))}
-              </tr>
-            ))}
-        </tbody>
-      </table>
-    );
   };
 
   return (
